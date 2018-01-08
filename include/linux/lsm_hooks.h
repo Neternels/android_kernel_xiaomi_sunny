b68d901eb85df48549d2195f07263cc5d38c3de8
Matthew Garrett <mjg59@google.com>
2018-01-08 13:36:19 -0800

    BACKPORT: security: Add a cred_getsecid hook
    For IMA purposes, we want to be able to obtain the prepared secid in the
    bprm structure before the credentials are committed. Add a cred_getsecid
    hook that makes this possible.
    
    Change-Id: If73fb5c6d4474cb6dde7ca8056f8b4c399c861fa
    Signed-off-by: Matthew Garrett <mjg59@google.com>
    Acked-by: Paul Moore <paul@paul-moore.com>
    Cc: Paul Moore <paul@paul-moore.com>
    Cc: Stephen Smalley <sds@tycho.nsa.gov>
    Cc: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: Mimi Zohar <zohar@linux.vnet.ibm.com>
    [cyberknight777: Move cred_getsecid to lsm_hook_defs.h]
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -572,6 +572,10 @@
  *	@new points to the new credentials.
  *	@old points to the original credentials.
  *	Transfer data from original creds to new creds
+ * @cred_getsecid:
+ *	Retrieve the security identifier of the cred structure @c
+ *	@c contains the credentials, secid will be placed into @secid.
+ *	In case of failure, @secid will be set to zero.
  * @kernel_act_as:
  *	Set the credentials for a kernel service to act as (subjective context).
  *	@new points to the credentials to be modified.
