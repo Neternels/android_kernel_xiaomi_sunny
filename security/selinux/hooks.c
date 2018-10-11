107d4cf1e21be6189ac6a5e8e5671ff2e8a45a22
Kees Cook <keescook@chromium.org>
2018-10-10 17:18:23 -0700

    UPSTREAM: LSM: Convert security_initcall() into DEFINE_LSM()
    Instead of using argument-based initializers, switch to defining the
    contents of struct lsm_info on a per-LSM basis. This also drops
    the final use of the now inaccurate "initcall" naming.
    
    Change-Id: Ief67d348af36e2d33d4e9e72a3b328c1ec00af67
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Reviewed-by: James Morris <james.morris@microsoft.com>
    Signed-off-by: James Morris <james.morris@microsoft.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -7075,7 +7075,9 @@ void set_selinux(int value)
 
 /* SELinux requires early initialization in order to label
    all processes and objects when they are created. */
-security_initcall(selinux_init);
+DEFINE_LSM(selinux) = {
+	.init = selinux_init,
+};
 
 #if defined(CONFIG_NETFILTER)
 
