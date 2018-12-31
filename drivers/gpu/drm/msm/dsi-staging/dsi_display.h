570ab3f3883459e069e71fd9badb440c861e99ff
DevriesL <therkduan@gmail.com>
2018-12-31 21:42:21 +0800

    drm: msm: dsi-staging: Add get_main_display
    Change-Id: Id670875adc18659521c424a716cf0ef194f9627f
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.h
@@ -720,4 +720,6 @@ int dsi_display_cmd_engine_enable(struct dsi_display *display);
 int dsi_display_cmd_engine_disable(struct dsi_display *display);
 int dsi_host_alloc_cmd_tx_buffer(struct dsi_display *display);
 
+struct dsi_display *get_main_display(void);
+
 #endif /* _DSI_DISPLAY_H_ */
