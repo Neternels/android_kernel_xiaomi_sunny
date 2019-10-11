119a5c75961def92fcb74558517010f2ed066e7f
Eric Dumazet <edumazet@google.com>
2019-10-10 20:17:38 -0700

    BACKPORT: tcp: add rcu protection around tp->fastopen_rsk
    Both tcp_v4_err() and tcp_v6_err() do the following operations
    while they do not own the socket lock :
    
    	fastopen = tp->fastopen_rsk;
     	snd_una = fastopen ? tcp_rsk(fastopen)->snt_isn : tp->snd_una;
    
    The problem is that without appropriate barrier, the compiler
    might reload tp->fastopen_rsk and trigger a NULL deref.
    
    request sockets are protected by RCU, we can simply add
    the missing annotations and barriers to solve the issue.
    
    Fixes: 168a8f58059a ("tcp: TCP Fast Open Server - main code path")
    Change-Id: Ic343bfeee8985f1105235407560340e5ecf66320
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2444,7 +2444,7 @@ bool tcp_schedule_loss_probe(struct sock *sk, bool advancing_rto)
 	/* Don't do any loss probe on a Fast Open connection before 3WHS
 	 * finishes.
 	 */
-	if (tp->fastopen_rsk)
+	if (rcu_access_pointer(tp->fastopen_rsk))
 		return false;
 
 	/* Schedule a loss probe in 2*RTT for SACK capable connections
