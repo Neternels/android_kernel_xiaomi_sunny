8b294a5ea80948f23e06cb2e7499a895b3f53d60
Nauval Rizky <enuma.alrizky@gmail.com>
2021-07-17 14:23:11 +0700

    msm: dsi-staging: Remove unnecessary primary display detection
    It wasn't even properly used in the first place anyways
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    [fiqri19102002: Adapt to current changes]
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
@@ -204,8 +204,7 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 
 	atomic_set(&c_bridge->display->panel->esd_recovery_pending, 0);
 
-	if (c_bridge->display->is_prim_display &&
-            atomic_read(&c_bridge->display_active)) {
+	if (atomic_read(&c_bridge->display_active)) {
 		cancel_delayed_work_sync(&c_bridge->pd_work);
 		return;
 	}
@@ -258,8 +257,7 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 	if (rc)
 		pr_err("Continuous splash pipeline cleanup failed, rc=%d\n",
 									rc);
-	if (c_bridge->display->is_prim_display)
-		atomic_set(&c_bridge->display_active, true);
+	atomic_set(&c_bridge->display_active, true);
 }
 
 int panel_disp_param_send(struct dsi_display *display, int cmd);
@@ -488,8 +486,7 @@ static void dsi_bridge_post_disable(struct drm_bridge *bridge)
 	msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK, &g_notify_data);
 	/*add for thermal end*/
 
-	if (c_bridge->display->is_prim_display)
-		atomic_set(&c_bridge->display_active, false);
+	atomic_set(&c_bridge->display_active, false);
 }
 
 #if CONFIG_TOUCHSCREEN_COMMON
@@ -1262,13 +1259,11 @@ struct dsi_bridge *dsi_drm_bridge_init(struct dsi_display *display,
 	encoder->bridge->is_dsi_drm_bridge = true;
 	mutex_init(&encoder->bridge->lock);
 
-	if (display->is_prim_display) {
-		atomic_set(&resume_pending, 0);
-		wakeup_source_init(&prim_panel_wakelock, "prim_panel_wakelock");
-		atomic_set(&bridge->display_active, false);
-		init_waitqueue_head(&resume_wait_q);
-		INIT_DELAYED_WORK(&bridge->pd_work, dsi_bridge_post_disable_work);
-	}
+	atomic_set(&resume_pending, 0);
+	wakeup_source_init(&prim_panel_wakelock, "prim_panel_wakelock");
+	atomic_set(&bridge->display_active, false);
+	init_waitqueue_head(&resume_wait_q);
+	INIT_DELAYED_WORK(&bridge->pd_work, dsi_bridge_post_disable_work);
 
 	return bridge;
 error_free_bridge:
