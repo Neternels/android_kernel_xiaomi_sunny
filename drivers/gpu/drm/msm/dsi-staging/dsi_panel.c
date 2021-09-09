b1b0cf7845b09b26fb4a1c6a04d9c6fdf1ccdf23
Cyber Knight <cyberknight755@gmail.com>
2021-09-09 16:06:32 +0800

    [SQUASH] reverted ULPS changes
    These may have tampered with thermal readings which shows the
    temperature as 3⁰C
    
    Revert "drivers: gpu: drm-msm: Enforce ULPS Suspend feature as well"
    
    This reverts commit 44588a3dc560b01d834a3bfa35a175b0cbbb1bde.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    Revert "drm/msm/dsi-staging: enable ULPS"
    
    This reverts commit 9987a03ad2c890b2cfbe34aa53bd12d9aaebeddf.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -2244,12 +2244,14 @@ static int dsi_panel_parse_misc_features(struct dsi_panel *panel)
 {
 	struct dsi_parser_utils *utils = &panel->utils;
 
-	panel->ulps_feature_enabled = true;
+	panel->ulps_feature_enabled =
+		utils->read_bool(utils->data, "qcom,ulps-enabled");
 
 	pr_info("%s: ulps feature %s\n", __func__,
 		(panel->ulps_feature_enabled ? "enabled" : "disabled"));
 
-	panel->ulps_suspend_enabled = true;
+	panel->ulps_suspend_enabled =
+		utils->read_bool(utils->data, "qcom,suspend-ulps-enabled");
 
 	pr_info("%s: ulps during suspend feature %s", __func__,
 		(panel->ulps_suspend_enabled ? "enabled" : "disabled"));
