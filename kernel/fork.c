6635c147860ffa264471cefcee33bbc87038f675
Cyber Knight <cyberknight755@gmail.com>
2021-10-03 01:13:01 +0800

    kernel: Boost CPU freq to max upon zygote forking according to set kernel profile
    Level 1 (battery) = no boosting
    Level 2 (balanced) = boost to max freq for 25ms
    Level 0 (default / Level 3 (performance) = boost to max freq for 50ms
    
    Inspired by: https://github.com/Panchajanya1999/msm-4.14/commit/8079275c2bb9d51a0b6715ac65e1e02d468edfe4
    
    Suggested-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -97,6 +97,7 @@
 #include <linux/simple_lmk.h>
 
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -2237,8 +2238,10 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost DDR bus to the max when userspace launches an app according to set kernel profile */
 	if (task_is_zygote(current) && active_mode() == 2) {
+	  cpu_input_boost_kick_max(25);
 	  devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 25);
 	} else if (task_is_zygote(current) && active_mode() == 0 || active_mode() == 3) {
+	  cpu_input_boost_kick_max(50);
 	  devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	} else if (task_is_zygote(current) && active_mode() == 1) {
 	  pr_info("Battery profile detected! Skipping DDR bus boost...\n");
