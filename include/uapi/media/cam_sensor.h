12c6fe9c81dfdd28ffafd836c04bb9ec4cbcb3ba
Cyber Knight <cyberknight755@gmail.com>
2021-12-25 01:11:42 +0800

    include: Convert all relevant TARGET_PROJECT to CONFIG_XIMI_MOJITO
    Fixes: 0e778c7 {"drivers: Convert all relevant TARGET_PROJECT to CONFIG_XIMI_MOJITO"}
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- include/uapi/media/cam_sensor.h
+++ include/uapi/media/cam_sensor.h
@@ -162,7 +162,7 @@ struct cam_cmd_probe {
 	uint32_t    data_mask;
 	uint16_t    camera_id;
 	uint16_t    reserved;
-	#ifdef CONFIG_TARGET_PROJECT_K7_CAMERA
+	#ifdef CONFIG_XIMI_MOJITO
 	//char	    sensorName[32];
 	#endif
 } __attribute__((packed));
