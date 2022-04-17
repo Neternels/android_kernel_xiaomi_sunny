ffc7c157be0314b50225e63832911b5a21cf1e86
Cyber Knight <cyberknight755@gmail.com>
2022-04-18 01:07:29 +0800

    cpufreq: schedutil: Expose default configuration options for all CPUs
    - Adapted from [1].
    
    [1]: https://github.com/RuRuTiaSaMa/android_kernel_xiaomi_sm8150/commit/885bcf1dbf455c0d74fed401b34eed4bab4f7be5
    
    Co-authored-by: RuRuTiaSaMa <1009087450@qq.com>
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1070,10 +1070,17 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us =
-				CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
-	tunables->down_rate_limit_us =
-	                        CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
+	switch(policy->cpu) {
+	default:
+	case 0:
+	  tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT_LP;
+	  tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_LP;
+	  break;
+	case 6:
+	  tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT_PERF;
+	  tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_PERF;
+	  break;
+	}
 	
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
