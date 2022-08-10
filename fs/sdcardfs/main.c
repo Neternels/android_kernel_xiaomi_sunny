aaa6753d62ae38a89b87268793041dabb62d29f9
Cyber Knight <cyberknight755@gmail.com>
2022-08-11 01:07:48 +0800

    sdcardfs: Don't initialize if is_inline is set to true
    - We use Project ID Quota on inline builds which drops the dependency of SDcardFS.
    - It is just pure overhead if kept enabled on Project ID Quota ROMs hence let's not initialize the driver when is_inline is set to true.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -469,9 +469,15 @@ static struct file_system_type sdcardfs_fs_type = {
 };
 MODULE_ALIAS_FS(SDCARDFS_NAME);
 
+extern bool is_inline;
 static int __init init_sdcardfs_fs(void)
 {
 	int err;
+
+	if (is_inline) {
+		pr_info("NetErnels: Inline ROM detected! Killing SDCARDFS...\n");
+		return 0;
+	}
 
 	pr_debug_ratelimited("Registering sdcardfs " SDCARDFS_VERSION "\n");
 
